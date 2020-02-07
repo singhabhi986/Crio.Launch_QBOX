859ae5791ecfe0b9143520b237333ef00b9111de
CrioUser <crio-user@criodo.com>
2020-02-07 06:07:00 +0000

    ME_QBOX_MODULE_LINUX_COMMANDS

--- linux_commands/print_home_directory.sh
+++ linux_commands/print_home_directory.sh
@@ -6,6 +6,6 @@
     # 2) If there are multiple commands, either put a semicolon between the commands or write each command on a separate line
     # 3) Execute the script before submission and confirm the output
 
-whoami
+
 pwd
 
