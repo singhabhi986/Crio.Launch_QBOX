16ce83b6c9017b7fd8ea158404d631ee2a825737
CrioUser <crio-user@criodo.com>
2020-02-07 06:40:54 +0000

    ME_QBOX_MODULE_LINUX_COMMANDS

--- linux_commands/print_home_directory.sh
+++ linux_commands/print_home_directory.sh
@@ -4,10 +4,13 @@
     # Tasks:
     # 1) Add commands to print the home directory for crio-user
     # 2) If there are multiple commands, either put a semicolon between the commands or write each command on a separate line
-    # 3) Execute the script before submission and confirm the output
+    # 3) Execute the script before submission and confirm the out
+
 
 cd ~/workspace
 cd ..
-cd ..
 pwd
+
+
+
 
